47dc17f71b9a0e9f192c8393498b97999ecb0616
Tatsiana <ignashchanka@gmail.com>
2021-05-02 20:18:49 +0300

    upgating heroku stack to heroku-20

--- config.py
+++ config.py
@@ -4,4 +4,7 @@ database_name = 'music'
 shelve_name = 'music\shelve.db'
 
 
-postgre_conn_string = "host='tantor.db.elephantsql.com' dbname='tsbgxbxm' user='tsbgxbxm' password='w0tmJEUr3lqERPmYnt5kDmEqUnilJRWL'"
+#postgre_conn_string = "host='tantor.db.elephantsql.com' dbname='tsbgxbxm' user='tsbgxbxm' password='w0tmJEUr3lqERPmYnt5kDmEqUnilJRWL'"
+
+
+postgre_conn_string = "host='ec2-54-90-13-87.compute-1.amazonaws.com' dbname='d9bsan98ui4ua8' user='nizfpspbzckvgk' password='a1c6d8aea3bb12bf2b1aefefc5e59d3798f25676b1db2faf8134102eba4bde5d'"
